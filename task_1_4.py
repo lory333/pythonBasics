9931c606f939a448825668c29812c902f87f36ea
lory333 <lory_godic@sluz.ch>
2025-09-16 15:21:23 +0200

    fertig

--- task_1_4.py
+++ task_1_4.py
@@ -1,19 +1,11 @@
+
 zahl1 = float(input("Bitte die erste Zahl eingeben: "))
 zahl2 = float(input("Bitte die zweite Zahl eingeben: "))
 
-# Berechnungen
 addition = round(zahl1 + zahl2, 2)
 subtraktion = round(zahl1 - zahl2, 2)
 multiplikation = round(zahl1 * zahl2, 2)
 
-# Division prüfen, um Division durch Null zu vermeiden
-if zahl2 != 0:
-    division = round(zahl1 / zahl2, 2)
-else:
-    division = "nicht definiert (Division durch 0)"
-
-# Ergebnisse ausgeben
 print("Addition:", addition)
 print("Subtraktion:", subtraktion)
 print("Multiplikation:", multiplikation)
-print("Division:", division)
